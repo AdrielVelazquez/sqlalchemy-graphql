5188646f9318306d1270b21d6133397aad28960c
Adriel Velazquez <AdrielVelazquez@gmail.com>
2016-04-03 17:04:50 -0700

    New Setup

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
-from setuptools import setup, find_packages
-
-
+from distutils.core import setup
+from setuptools import find_packages
 
 setup(
     name="sqlalchemy_graphql",
-    version="0.1.1",
+    version="1.0",
     description="GraphQL extension for dealing with SQLAlchemy",
     long_description=open("README.rst", "r").read(),
     author="Adriel Velazquez",
